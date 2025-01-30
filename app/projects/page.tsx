f687d1a311db2fb5d8a649dab80821496131667b
Robert Schlittler <robert.schlittler@gmx.ch>
2025-01-30 15:42:49 +0100

    improve styling

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import { FileUpload } from '@/components/ui/file-upload/FileUpload';
 
 export default function UploadPage() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-indigo-900/30 text-white py-12">
+    <main className="min-h-screen">
       <FileUpload />
     </main>
   );
